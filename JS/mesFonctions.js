c940349311253dbd0838cdc1fb688e3f896b13fe
Thomas LACHEHEB <thomas.lacheheb@hotmail.fr>
2018-10-23 14:58:00 +0200

    Fusion Branch Thomas & Master pour Minh

--- JS/mesFonctions.js
+++ JS/mesFonctions.js
@@ -26,7 +26,7 @@ function openWindowOffre(idOffre, nomService, descriptionOffre, dateOffre)
     (
         {
             type:"get",
-            url:"index.php/Ctrl_Acceuil/addInformationOffre",
+            url:"http://localhost/BTS2/PPE3_troc/index.php/Ctrl_Acceuil/addInformationOffre",
             data:"idOffre="+idOffre+"&nomServ="+nomService+"&descOffre="+descriptionOffre+"&dateOffre="+dateOffre,
             success:function(data)
             {
@@ -53,7 +53,7 @@ function updateOffre()
     (
         {
             type:"get",
-            url:"index.php/Ctrl_Acceuil/setInformationOffre",
+            url:"http://localhost/BTS2/PPE3_troc/index.php/Ctrl_Acceuil/setInformationOffre",
             data:"idOffre="+idOffre+"&nomServ="+nomService+"&descOffre="+descOffre+"&dateOffre="+dateOffre,
             success:function(data)
             {
@@ -74,7 +74,7 @@ function openWindowDemande(idDemande, nomService, descriptionDemande, dateDemand
     (
         {
             type:"get",
-            url:"index.php/Ctrl_Acceuil/addInformationDemande",
+            url:"http://localhost/BTS2/PPE3_troc/index.php/Ctrl_Acceuil/addInformationDemande",
             data:"idDemande="+idDemande+"&nomServ="+nomService+"&descDemande="+descriptionDemande+"&dateDemande="+dateDemande,
             success:function(data)
             {
@@ -101,7 +101,7 @@ function updateDemande()
     (
         {
             type:"get",
-            url:"index.php/Ctrl_Acceuil/setInformationDemande",
+            url:"http://localhost/BTS2/PPE3_troc/index.php/Ctrl_Acceuil/setInformationDemande",
             data:"idDemande="+idDemande+"&nomServ="+nomService+"&descDemande="+descDemande+"&dateDemande="+dateDemande,
             success:function(data)
             {
